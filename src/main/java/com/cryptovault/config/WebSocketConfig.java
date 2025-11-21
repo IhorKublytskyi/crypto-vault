df97bd73518980e02608a4363d87aee5a37d31e5
Anton Hryshchuk <grutoha@gmail.com>
2025-11-21 09:16:34 +0100

    add spring profiles (dev/prod) and websocket config

--- src/main/java/com/cryptovault/config/WebSocketConfig.java
+++ src/main/java/com/cryptovault/config/WebSocketConfig.java
@@ -13,13 +13,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
+
         config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*")
-                .withSockJS();
+        registry.addEndpoint("/ws-cryptovault").setAllowedOriginPatterns("*").withSockJS();
     }
 }
